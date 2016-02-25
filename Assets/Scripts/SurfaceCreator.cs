1bec4d7bd972f4464d5a5071b6ec919cd2d2e1a5
Antonio Nikolov <anikolovn@gmail.com>
2016-02-25 12:02:11 +0000

    catlike Noise tutorial

---
+++ Assets/Scripts/SurfaceCreator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SurfaceCreator : MonoBehaviour
+{
+
+    private Mesh mesh;
+
+    private void OnEnable()
+    {
+        if (mesh == null)
+        {
+            mesh = new Mesh();
+            mesh.name = "Surface Mesh";
+        }
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+    }
+}
